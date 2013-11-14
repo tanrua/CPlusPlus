4b48f4ee8a10b1e952b72c14da8275287bb1a9b5
tanrua <redheadgiant@gmail.com>
2013-11-14 18:16:43 -0500

    moar updates

--- src/group.h
+++ src/group.h
@@ -10,62 +10,63 @@ using namespace std;
 
 template <class T> class Group {
         int d_size;
-	T d_label;
-	int d_age;
+		T d_label;
+		int d_age;
         set<pair<char, int > > tiles;
 public:
-	Group(int _size, T _label): d_size(_size), d_label(_label) {};
+		Group(T _label, int _age=0, int _size=0): d_size(_size), d_label(_label), d_age(_age) {};
 
-                //Not sure WTF this is supposed to do...
-                T getDummy(){
-                        return 0;
-                }
+        //Not sure WTF this is supposed to do...
+        T getDummy(){
+                return 0;
+        }
 
-                //add the given tile to the group
-				void add(const unsigned char& col, const int& row){
-                        add(make_pair(col, row));
-                }
+        //add the given tile to the group
+		void add(const unsigned char& col, const int& row){
+                add(make_pair(col, row));
+        }
 
-                //Add the given tile to the group
-                void add(std::pair<char, int>& tile){
-                        tiles.insert(tile) ;
-                        d_size++ ;
-                }
+        //Add the given tile to the group
+        void add(const std::pair<char, int>& tile){
+                tiles.insert(tile) ;
+                d_size++ ;
+        }
 
-                //Check is this group contains the given tile
-                bool contains(const char& col, const int& row) {
-                        return contains(make_pair(col, row_)) ;
-                }
+        //Check if this group contains the given tile
+        bool contains(const char& col, const int& row) {
+                return contains(make_pair(col, row)) ;
+        }
 
-                //Check is this group contains the given tile
-                bool contains(pair<char, int>& tile) {
-                        return tiles.count(tile) ;
-                }
-				              
-                //Copy over the contents of the other group
-                void addAll(const Group& other) {
-                        if (other.size() > 0) {
-                                //iterate through other and add to this
-                                for (set<pair<char, int>::iterator it=other.tiles.begin(); it!=other.tiles.end(); ++it){
-                                        add(*it) ;
-                                }
+        //Check if this group contains the given tile
+        bool contains(pair<char, int>& tile) {
+                return tiles.count(tile) ;
+        }
+		              
+        //Copy over the contents of the other group
+        void addAll(const Group& other) {
+                if (other.size() > 0) {
+                        //iterate through other and add to this
+                        for (set<pair<char, int>::iterator it=other.tiles.begin(); it!=other.tiles.end(); ++it){
+                                add(*it) ;
                         }
                 }
-				/************************************ Getters and Setters *****************************************************/
-				//return the size of this group
-                int getSize() {
-                        return d_size ;
-                }
+        }
+		/************************************ Getters and Setters *****************************************************/
+		//return the size of this group
+        int& getSize() {
+                return d_size ;
+        }
 
-				//return what turn the group was created on
-                int getAge() {
-					return d_age ;
-                }
+		//return what turn the group was created on
+        int& getAge() {
+			return d_age ;
+        }
 
-                //Return the label of this group
-                T getLabel() {
-                        return d_label ;
-                }
+        //Return the label of this group
+        T& getLabel() {
+                return d_label ;
+        }
+
 };
 
 class Position {
