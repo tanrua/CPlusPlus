8952cae6d6a3018eacf2dc2602a291808ce93ca8
tanrua <redheadgiant@gmail.com>
2013-11-27 19:18:25 -0500

    moved position out and fexed some other stuff

--- src/group.h
+++ src/group.h
@@ -14,33 +14,33 @@ template <class T> class Group {
 		int d_age;
         set< pair<char, int > > d_tiles;
 public:
-		Group(T _label, int _age=0, int _size=0): d_label(_label), d_age(_age), d_size(_size){};
+		Group(const T& _label, const int& _age=0, const int& _size=0): d_label(_label), d_age(_age), d_size(_size){};
 
         //Not sure WTF this is supposed to do...
         T getDummy(){
 			if (typeid(T) == typeid(int)) {
-                return 0;
+                return 666;
 			} else if (typeid(T) == typeid(long)) {
-                return 0;
+                return 666;
 			} else if (typeid(T) == typeid(unsigned char)) {
                 return '+';
 			} else if (typeid(T) == typeid(char)) {
 				return '+';
 			} else {
-				return 0;
+				return 666;
 			}
-        }
+        };
 
         //add the given tile to the group
 		void add(const unsigned char& col, const int& row){
                 add(make_pair(col, row));
-        }
+        };
 
         //Add the given tile to the group
         void add(const pair<char, int>& tile){
                 d_tiles.insert(tile) ;
                 d_size++ ;
-        }
+        };
 		              
         //Copy over the contents of the other group
         void addAll(const Group<T>& other) {
@@ -50,22 +50,22 @@ public:
                            add(*it) ;
                    }
            }
-        }
+        };
 
         //Check if this group contains the given tile
         bool contains(const char& col, const int& row) {
                 return contains(make_pair(col, row)) ;
-        }
+        };
 
         //Check if this group contains the given tile
         bool contains(const pair<char, int>& tile) {
                 return d_tiles.count(tile) ;
-        }
+        };
 
 		//remove the given tile from group
 		void remove(const unsigned char& col, const int& row){
 			remove(make_pair(col, row));
-        }
+        };
 
         //remove the given tile from groupp
         void remove(const pair<char, int>& tile){
@@ -74,52 +74,43 @@ public:
 				d_tiles.erase(tile);
                 d_size-- ;
 			}
-        }
+        };
 
 		/************************************ Getters and Setters *****************************************************/
 		//return the size of this group
         int& getSize() {
                 return d_size ;
-        }
+        };
 
 		//return what turn the group was created on
         int& getAge() {
 			return d_age ;
-        }
+        };
 
         //Return the label of this group
         T& getLabel() {
                 return d_label ;
-        }
+        };
 
 		//return the size of this group
         void setSize(const int& _size) {
                 d_size=_size;
-        }
+        };
 
 		//return what turn the group was created on
         void setAge(const int& _age) {
 			d_age=_age;
-        }
+        };
 
         //Return the label of this group
         void setLabel(const T& _label) {
                 d_label=_label;
-        }
+        };
 
 		void print(){
 			cout << "Group '" << d_label << "' is of size: " << d_size << endl;
-		}
+		};
 
-};
-
-class Position {
-	unsigned char d_x;
-	int d_y;
-public:
-	Position(unsigned char _x = 'a', int _y = 0): d_x(_x), d_y(_y){};
-	unsigned char getX(){ return d_x;};
-	int getY() { return d_y;};
 };
 
 #endif
